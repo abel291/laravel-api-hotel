ee27e9c662504c567668f333a8db5d4c0f2a1874
Abel <abel291m@gmail.com>
2021-04-01 22:36:24 -0500

    primer commit desde consola

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,14 @@
+<x-app-layout>
+    <x-slot name="header">        
+            {{ __('Dashboard') }}        
+    </x-slot>
+    
+    <div class="pb-6">
+        <div class="max-w-7xl mx-auto sm:px-4 lg:px-8">
+            <div class="bg-white overflow-hidden shadow sm:rounded p-6 h-full">
+                <h1>caja</h1>
+            </div>            
+        </div>
+    </div>
+</div>
+</x-app-layout>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+
+        @livewireStyles
+
+        <!-- Scripts -->
+        <script src="{{ mix('js/app.js') }}" defer></script>
+    </head>
+    <body class="font-sans antialiased bg-gray-100">
+        <div class="md:flex flex-col md:flex-row md:min-h-screen w-full"  >
+            
+            <div x-data="{ open: false }" class="sidebar-nabvar md:w-64 bg-gray-800 flex-shrink-0 flex-col space-y-1 text-white text-sm text-gray-300  ">
+                
+                <div class="max-w-7xl mx-auto px-4 bg-gray-900 h-16">
+                    
+                    <div class="flex  md:justify-center justify-between items-center h-16 ">
+                        
+                        <div class=" flex  text-2xl font-semibold text-white justify-start  md:justify-center" >
+                            <a href="!#" >
+                                RRHH
+                            </a>
+                        </div>
+
+                        <div class="-mr-2 flex  md:hidden">
+                            <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-100 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
+                                <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                                    <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                                    <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                                </svg>
+                            </button>
+                        </div>
+                        
+                    </div>
+                    
+                </div>
+                <div :class="{'block': open, 'hidden': ! open}" class="px-2 py-3 hidden hidden md:block">
+                    <a href="" class="flex items-center hover:bg-gray-900 hover:text-white rounded-md p-2.5 space-x-3" >
+                        
+                        <svg  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+                        </svg>
+                        <span>Dashboard</span>
+                    </a>
+                    <a href="" class="flex items-center hover:bg-gray-900 hover:text-white rounded-md p-2.5 space-x-3" >
+                        <svg  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+                        </svg>
+                        <span>Clientes</span>
+                    </a>
+                    <a href="" class="flex items-center hover:bg-gray-900 hover:text-white rounded-md p-2.5 space-x-3" >
+                        <svg  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4" />
+                        </svg>
+                        <span>Reservaciones</span>
+                    </a>
+                    <a href="" class="flex items-center hover:bg-gray-900 hover:text-white rounded-md p-2.5 space-x-3" >
+                        <svg  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4" />
+                        </svg>
+                        <span>Habitaciones</span>
+                    </a>
+                    <a href="" class="flex items-center hover:bg-gray-900 hover:text-white rounded-md p-2.5 space-x-3"  >    
+                        <svg      xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 15.546c-.523 0-1.046.151-1.5.454a2.704 2.704 0 01-3 0 2.704 2.704 0 00-3 0 2.704 2.704 0 01-3 0 2.704 2.704 0 00-3 0 2.704 2.704 0 01-3 0 2.701 2.701 0 00-1.5-.454M9 6v2m3-2v2m3-2v2M9 3h.01M12 3h.01M15 3h.01M21 21v-7a2 2 0 00-2-2H5a2 2 0 00-2 2v7h18zm-3-9v-2a2 2 0 00-2-2H8a2 2 0 00-2 2v2h12z" />
+                        </svg>   
+                        <span>Experiencias</span>
+                    </a>
+                    <a href="" class="flex items-center hover:bg-gray-900 hover:text-white rounded-md p-2.5 space-x-3"  >    
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                        </svg>  
+                        <span>Galeria</span>
+                    </a>
+                    <a href="" class="flex items-center hover:bg-gray-900 hover:text-white rounded-md p-2.5 space-x-3"  >    
+                        <svg      xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 15.546c-.523 0-1.046.151-1.5.454a2.704 2.704 0 01-3 0 2.704 2.704 0 00-3 0 2.704 2.704 0 01-3 0 2.704 2.704 0 00-3 0 2.704 2.704 0 01-3 0 2.701 2.701 0 00-1.5-.454M9 6v2m3-2v2m3-2v2M9 3h.01M12 3h.01M15 3h.01M21 21v-7a2 2 0 00-2-2H5a2 2 0 00-2 2v7h18zm-3-9v-2a2 2 0 00-2-2H8a2 2 0 00-2 2v2h12z" />
+                        </svg>   
+                        <span>Servicios</span>
+                    </a>
+                </div>
+
+             
+                
+                
+            </div>
+            <div class="w-full">
+                @livewire('navigation-dropdown')
+
+                <!-- Page Heading -->
+                <header class="">
+                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 ">
+                        <h2 class="font-bold text-2xl text-gray-800 ">
+                        {{ $header }}
+                        </h2>
+                    </div>
+                </header>
+
+                <!-- Page Content -->
+                <main>
+                    {{ $slot }}
+                </main>
+            </div>
+        </div>
+
+        @stack('modals')
+
+        @livewireScripts
+    </body>
+</html>
